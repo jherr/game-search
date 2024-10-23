5abee6eead37fae7d6d9b82a3de452439d73e2ca
Jack Herrington <jherr@pobox.com>
2024-10-22 20:04:12 -0700

    Added tailwind

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -1,31 +1,33 @@
 // app/routes/__root.tsx
-import { createRootRoute } from '@tanstack/react-router'
-import { Outlet, ScrollRestoration } from '@tanstack/react-router'
-import { Body, Head, Html, Meta, Scripts } from '@tanstack/start'
-import * as React from 'react'
+import { createRootRoute } from "@tanstack/react-router";
+import { Outlet, ScrollRestoration } from "@tanstack/react-router";
+import { Body, Head, Html, Meta, Scripts } from "@tanstack/start";
+import * as React from "react";
+
+import "./index.css";
 
 export const Route = createRootRoute({
   meta: () => [
     {
-      charSet: 'utf-8',
+      charSet: "utf-8",
     },
     {
-      name: 'viewport',
-      content: 'width=device-width, initial-scale=1',
+      name: "viewport",
+      content: "width=device-width, initial-scale=1",
     },
     {
-      title: 'TanStack Start Starter',
+      title: "TanStack Start Starter",
     },
   ],
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <RootDocument>
       <Outlet />
     </RootDocument>
-  )
+  );
 }
 
 function RootDocument({ children }: { children: React.ReactNode }) {
@@ -34,11 +36,11 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       <Head>
         <Meta />
       </Head>
-      <Body>
+      <Body className="bg-gray-100">
         {children}
         <ScrollRestoration />
         <Scripts />
       </Body>
     </Html>
-  )
+  );
 }
